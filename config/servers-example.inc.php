b3094c6a42e10ca600510e64d1f6baa2655d3e97
Prowl <prowlcorp@gmail.com>
2020-11-29 09:01:35 -0600

    a

---
+++ config/servers-example.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$PokemonServers = array (
+  'showdown' =>
+  array (
+    'name' => 'Smogon University',
+    'id' => 'showdown',
+    'server' => 'sim.psim.us',//PROWL change
+    'port' => 8000,
+  ),
+);
