3b345c7a2c40efd6707c0bd67d10b26b0ed6ccc6
Prowl <prowlcorp@gmail.com>
2020-11-29 10:48:45 -0600

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -19,7 +19,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: 'http://pokemon-showdown-custom.herokuapp.com', //PROWL change
+	host: 'pokemon-showdown-custom.herokuapp.com', //PROWL change
 	port: 443,
 	httpport: 8000,
 	altport: 80,
